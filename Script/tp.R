4249713bf11e127e221189eaf3aade9ea0182e0b
paulaluvini <pluvini@udesa.edu.ar>
2021-05-01 12:34:13 -0300

    Primeros plots II

--- Script/tp.R
+++ Script/tp.R
@@ -4,9 +4,7 @@ library(ggplot2)
 library(sf)
 library('Hmisc')
 
-#getwd()
-
-#Leemos los archivos a utilizar
+# Primero leemos los archivos a utilizar
 departamentos <- st_read("Datos/Codgeo_Pais_x_dpto_con_datos/pxdptodatosok.shp")
 smn <- read.csv(file = 'Datos/estaciones_smn.csv', sep = ";")
 
@@ -32,6 +30,7 @@ describe(smn)
 
 # Vemos cómo se distribuyen las estaciones por provincia.
 # La provincia con mayores estaciones es, por lejos, Buenos Aires. La siguen Córdoba y Santa Fe.
+
 View(smn %>% group_by(Provincia) %>% tally(sort = TRUE))
 
 # Acá tengo que sumarle los minutos a la latitud porque vienen separados
