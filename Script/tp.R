711365e47b695a8ddc5df240f97601b34e452ee4
paulaluvini <pluvini@udesa.edu.ar>
2021-05-01 11:02:03 -0300

    1

--- Script/tp.R
+++ Script/tp.R
@@ -1,4 +1,50 @@
-getwd()
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(sf)
 
-smn <- read.csv(file = 'Datos/estaciones_smn2.csv')
+#getwd()
+
+#Leemos los archivos a utilizar
+departamentos <- st_read("Datos/Codgeo_Pais_x_dpto_con_datos/pxdptodatosok.shp")
+smn <- read.csv(file = 'Datos/estaciones_smn.csv', sep = ";")
 View(smn)
+
+#Primero miro un poco el de departamentos que utilizamos en clase.
+summary(departamentos)
+grafico1 <-ggplot() + geom_sf(data =departamentos)
+grafico1
+summary(departamentos$provincia)
+
+#La Antartida es parte de T. del Fuego
+departamentos_tf <- departamentos %>% 
+  filter(provincia == "Tierra del Fuego") %>% mutate(personas = personas%>% as.numeric())
+grafico2 <- ggplot() + geom_sf(data =departamentos_tf, aes(fill = personas))
+grafico2
+
+#Ahora voy a ver la localización de las estaciones
+summary(smn)
+colnames(smn)
+#Cambio este nombre para que se lea mejor
+smn <- smn %>% rename(Estacion = ï..NOMBRE) 
+summary(smn$Provincia)
+
+#Acá tengo que sumarle los minutos a la latitud porque vienen separados
+
+smn$minutos_latitud <- smn$Latitud.min./60
+smn$minutos_longitud <- smn$Longitud.min./60
+head(smn)
+smn$x <- smn$Longitud.gr.-smn$minutos_longitud
+smn$y <- smn$Latitud.gr.-smn$minutos_latitud
+head(smn)
+
+ggplot() + geom_sf(data = departamentos) + geom_point(data = smn, aes(x=x, y=y),
+                                                      colour = "dodgerblue1", size = 1)+ theme_classic()
+
+departamentos_ba <- departamentos %>% filter(provincia == "Buenos Aires")
+smn_ba <- smn %>% filter(Provincia == "BUENOS AIRES")
+
+ggplot() + geom_sf(data = departamentos_ba) + 
+  geom_point(data = smn_ba, aes(x=x, y=y),colour = "dodgerblue1", size = 1)+ 
+  theme_classic() +geom_text(label= 'Estacion')#,hjust=0, vjust=0)
+
